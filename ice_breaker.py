66479157c73d846cc6430e822c6f5282f022842b
Kshitij Vengurlekar <writetokshitij@gmail.com>
2024-07-17 11:06:57 +0530

    Linkedin URL fetched, Information fetched

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,16 +1,16 @@
+from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
-# from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-information = """
+def ice_break_with(name: str) -> str:
+    linkedin_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
 
-"""
-
-
-if __name__ == '__main__':
     summary_template = """
         given the linkedin information {information} about a person. I want you to create:
         1. a short summary
@@ -19,14 +19,15 @@ if __name__ == '__main__':
 
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    llm = ChatOllama(model="llama3")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = summary_prompt_template | llm | StrOutputParser()
-    # This creates a processing chain by combining the prompt template and the language model.
-    # The | operator indicates that the output of the prompt template will be the input for the language model.
 
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json")
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+if __name__ == '__main__':
+    load_dotenv()
+    print("Ice breaker enter")
+    ice_break_with(name="Eden Marco")
